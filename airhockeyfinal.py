d808d750ee4a3a7ae59d1d3953aed4beed04525d
Noah Zweben <noahzweben@gmail.com>
2015-09-28 21:47:15 -0400

    try:

--- airhockeyfinal.py
+++ airhockeyfinal.py
@@ -7,6 +7,7 @@ import numpy as np
 cv2.namedWindow('game')
 cv2.createTrackbar('Velocity', 'game',1,100,nothing) #Assigns the velocity trackbar to game window
 def nothing(x):
+	x=1
 	pass
 
 #in x,y
